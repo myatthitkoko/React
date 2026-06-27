7db79c6a97ef8c1f3ca5be7919d5576792048a10
Myat Thit Ko Ko <github@myatthitkoko.com>
2026-06-27 23:36:29 +0000

    First steps for React

---
+++ weather-React-ver/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>Hello React</h1>
+      <p>This is my first React statement.</p>
+      <p>I can't believe how it refreshes automatically</p>
+    </>
+  )
+}
+
+export default App
