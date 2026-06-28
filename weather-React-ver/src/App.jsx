58125b7d935b25a889a8cd3ce34d7419b83836cb
Myat Thit Ko Ko <github@myatthitkoko.com>
2026-06-28 08:41:55 +0000

    minor changes to test functions

--- weather-React-ver/src/App.jsx
+++ weather-React-ver/src/App.jsx
@@ -1,9 +1,17 @@
 function App() {
+
+  function greet(name) {
+    alert(`Hi, how are you ${name}?`);
+  }
+
+  let name = "Stephen";
+
   return (
     <>
-      <h1>Hello React</h1>
+      <h1>Hello {name}</h1>
       <p>This is my first React statement.</p>
       <p>I can't believe how it refreshes automatically</p>
+      <button onClick={() => greet(name)}>My first button!</button>
     </>
   )
 }
