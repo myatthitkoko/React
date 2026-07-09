b198ccd617c4cd133ca15f6f529fbf195e9099e3
Myat Thit Ko Ko <github@myatthitkoko.com>
2026-07-09 06:24:58 +0000

    attempt to fix autoplay issue

--- NailsByJess/src/components/Hero.jsx
+++ NailsByJess/src/components/Hero.jsx
@@ -1,17 +1,29 @@
 import styles from '../styleSheets/Home.module.css'
+import { useEffect, useRef } from "react";
 
 export default function Hero() {
+    const videoRef = useRef(null);
+
+    useEffect(() => {
+        const video = videoRef.current;
+
+        if (video) video.play()
+    }, []);
+
     return (
         <>
             <div className={styles.Hero}>
                 <video
-                    src="/NailVid1.mp4"
+                    ref={videoRef}
                     autoPlay
                     muted
                     loop
                     playsInline
+                    preload="auto"
                     className={styles.myVideo}
-                />
+                >
+                    <source src="/NailVid1.mp4" type="video/mp4" />
+                </video>
                 <div className={styles.overlay}>
                     <h2 className={styles.title}>Luxury at your fingertips</h2>
                 </div>
