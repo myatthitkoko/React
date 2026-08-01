bef47c122ecb6520b1a79690138732b3432500c3
Myat Thit Ko Ko <github@myatthitkoko.com>
2026-08-01 09:09:25 +0000

    name change to match railway variables

--- NailsByJess/server/db/connection.js
+++ NailsByJess/server/db/connection.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
     password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT,
+    port: process.env.MYSQLPORT,
     waitForConnections: true,
     connectionLimit: 10,
 });
