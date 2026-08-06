34457e16d810a395a5b77a2504c7247833101d0c
Myat Thit Ko Ko <github@myatthitkoko.com>
2026-08-06 07:59:03 +0000

    email error handling

--- NailsByJess/server/emailAPI.js
+++ NailsByJess/server/emailAPI.js
@@ -6,12 +6,18 @@ dotenv.config();
 const resend = new Resend(process.env.MAILKEY);
 
 export async function sendMail(destination) {
-    resend.emails
-    .send({
-    from: 'onboarding@resend.dev',
-    to: destination,
-    subject: 'Test Email',
-    html: '<p>Congrats on successfully sending your <strong>email</strong>!</p>'
-    })
-    .catch(console.error);
+    try {
+        const result = await resend.emails
+        .send({
+        from: 'onboarding@resend.dev',
+        to: destination,
+        subject: 'Test Email',
+        html: '<p>Congrats on successfully sending your <strong>email</strong>!</p>'
+        });
+
+        console.log("Mail Result: ", result);
+    } catch (err) {
+        console.error("Mail failed: ", err);
+    }
+    
 }
