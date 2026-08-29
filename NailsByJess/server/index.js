6233b5066ad7f1b303b1919285a84f42005862ea
Myat Thit Ko Ko <github@myatthitkoko.com>
2026-08-29 06:56:49 +0000

    commented out one-time auth

--- NailsByJess/server/index.js
+++ NailsByJess/server/index.js
@@ -226,7 +226,7 @@ app.post("/api/booking", async (req, res) => {
 
 });
 
-/*One-Time Authorization for Refresh Token*/
+/*One-Time Authorization for Refresh Token
 app.get("/api/google/auth", (req, res) => {
   console.log("GOOGLE CLIENT ID:", process.env.GOOGLE_CLIENT_ID);
   console.log("GOOGLE REDIRECT URI:", process.env.GOOGLE_REDIRECT_URI);
@@ -268,7 +268,7 @@ app.get("/api/google/callback", async (req, res) => {
     res.status(500).send("Google Calendar authorization failed.");
   }
 });
-
+*/
 
 const PORT = process.env.PORT || 3000;
 
