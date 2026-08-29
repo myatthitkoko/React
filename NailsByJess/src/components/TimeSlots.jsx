b4df98386c173b21e31b7e52047dde0e778078e5
Myat Thit Ko Ko <github@myatthitkoko.com>
2026-08-29 10:17:31 +0000

    time slot UI fix

--- NailsByJess/src/components/TimeSlots.jsx
+++ NailsByJess/src/components/TimeSlots.jsx
@@ -5,21 +5,23 @@ export default function TimeSlots({availability, slot, onSlotSelect}) {
         <>
             <h2>Available Time Slots</h2> 
             <p>Please select a time slot to continue</p>
-            {availability.map((timeObject) => (
-                <div className={styles.timeSlots} key={timeObject.value}>
-                    <input 
-                    type='radio' 
-                    name='time' 
-                    id={timeObject.display} 
-                    value={timeObject.value} 
-                    checked = {slot === timeObject.value}
-                    onChange={() => onSlotSelect(timeObject.value)}
-                    />
-                    <label htmlFor={timeObject.display}>
-                        {timeObject.display}
-                    </label>
-                </div>
-            ))}
+            <div className={styles.timeSlots}>
+                {availability.map((timeObject) => (
+                    <div key={timeObject.value}>
+                        <input 
+                        type='radio' 
+                        name='time' 
+                        id={timeObject.display} 
+                        value={timeObject.value} 
+                        checked = {slot === timeObject.value}
+                        onChange={() => onSlotSelect(timeObject.value)}
+                        />
+                        <label htmlFor={timeObject.display}>
+                            {timeObject.display}
+                        </label>
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
